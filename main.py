7a4fa8eb372f4bc080d89c34095facaa798694f0
codemaker123 <30541820+codemaker123@users.noreply.github.com>
2022-06-16 21:54:13 +0800

    增加批量文件修改方法

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# git提交代码跳过ssl验证
+##git config --global http.sslVerify false
+import os
+import numpy as np
+import cv2 as cv
+import shutil
+from BatchRename import BatchRename
+
 
 
 def print_hi(name):
@@ -9,8 +17,30 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    demo = BatchRename()
+    demo.rename()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+# from matplotlib import pyplot as plt
+#
+# img = cv.imread('test.jpg',0)
+# edges = cv.Canny(img, 100, 200)
+#
+# plt.subplot(121),plt.imshow(img,cmap = 'gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+#
+# plt.show()
+
